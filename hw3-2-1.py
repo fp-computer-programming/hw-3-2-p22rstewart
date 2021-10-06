687048db60a50177e138f2f20c4016bbfcd48b9d
p22rstewart <p22rstewart@fairfieldprep.org>
2021-10-06 11:09:53 -0400

    hw

---
+++ hw3-2-1.py
@@ -0,0 +1,25 @@
+# Author: Ryan (AMDG) 10/6/21
+
+grade = int(input("input your grade: "))
+if grade < 60:
+    print("F")
+elif grade <= 64:
+    print("D")
+elif grade <= 69:
+    print("D+")
+elif grade <= 72:
+    print("C-")
+elif grade <= 76:
+    print("C")
+elif grade <= 79:
+    print("C+")
+elif grade <= 82:
+    print("B-")
+elif grade <= 86:
+    print("B")
+elif grade <= 89:
+    print("B+")
+elif grade <= 92:
+    print("A-")
+elif grade <= 100:
+    print("A")
---
+++ hw3-2-2.py
@@ -0,0 +1,17 @@
+# Author: Ryan (AMDG) 10/6/21
+# weight in kg height in m
+
+weight = int(input("Enter weight: "))
+height = int(input("Enter height: "))
+
+bmi = weight / (height ** 2)
+if bmi < 19:
+    print("you are underweight")
+elif bmi < 25:
+    print("you are healthy")
+elif bmi < 30:
+    print("you are overweight")
+elif bmi < 40:
+    print("you are obese")
+elif bmi >= 40:
+    print("you are extremely obese")
